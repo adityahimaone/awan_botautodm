e6521084f7070f8ebddfcb5e8ab46b7455315da3
Aditya Himawan <adityahimaone@users.noreply.github.com>
2020-09-23 14:19:21 +0700

    Build 3.0

--- app.py
+++ app.py
@@ -21,31 +21,36 @@ def start():
                     # lol is the keyword
                     # if you want to turn off the case sensitive like: lol , Lol, LOL
                     # just use lower(message) and check it, but please remove the replace function line
-                    if "lol" or "LOL" or "Lol" in message:
+                    if "quotes" in message:
+                        print("quotes keyword found, quotes will be uploud")
+                        message = message.replace("quotes", "")
+                        if len(message) != 0:
+                             if "https://" not in message and "http://" not in message:
+                                if "--s" in message:
+                                        message = message.replace("--s", "")
+                                        screen_name = tw.get_user_screen_name(sender_id)
+                                        media.download_image()
+                                        media.process_image(message, screen_name)
+                                        tw.post_quotes()
+                                        tw.delete_dm(id)
+                                else:
+                                        media.download_image()
+                                        media.process_image(message, None)
+                                        tw.post_quotes()
+                                        tw.delete_dm(id)
+                             else:
+                                tw.delete_dm(id)
+                        else:
+                            print("DM deleted because its empty..")
+                            tw.delete_dm(id)
+                    elif "lol" in message:
                         print("lol keyword found")
                        # message = message.replace("https://", "")
                        # message = message.replace("lol", "")
                         if len(message) != 0:
                             if dms[i]['media'] is None:
-                                if "quotes" in message:
-                                    print("quotes keyword found, quotes will be uploud")
-                                    message = message.replace("quotes", "")
-                                    if "https://" not in message and "http://" not in message:
-                                        if "--s" in message:
-                                            message = message.replace("--s", "")
-                                            screen_name = tw.get_user_screen_name(sender_id)
-                                            media.download_image()
-                                            media.process_image(message, screen_name)
-                                            tw.post_quotes()
-                                            tw.delete_dm(id)
-                                        else:
-                                            media.download_image()
-                                            media.process_image(message, None)
-                                            tw.post_quotes()
-                                            tw.delete_dm(id)
-                                    else:
-                                        tw.delete_dm(id)
-                                else:
+
+
                                     print("DM will be posted")
                                     tw.post_tweet(message)
                                     tw.delete_dm(id)
@@ -59,8 +64,12 @@ def start():
                             tw.delete_dm(id)
 
                     else:
-                        print("DM will be deleted because does not contains keyword..")
-                        tw.delete_dm(id)
+                        print("DM Not Constain keyword, SKIP")
+                        dms = tw.read_dm()
+                        if len(dms) == 0 or dms is None:
+                            time.sleep(10)
+                        #print("DM will be deleted because does not contains keyword..")
+                        #tw.delete_dm(id)
 
             dms = list()
 
